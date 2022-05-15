4e795ba7612d4824ee476af8a50a07970e2f6ada
VinothGnanasekaran <imverypsl4u1@gmail.com>
2022-05-15 20:12:07 +0530

    Edits to Frames Class & New commit of Selectable Class

--- SeleniumBasicsAutomation/src/learnSelenium/FramesExample.java
+++ SeleniumBasicsAutomation/src/learnSelenium/FramesExample.java
@@ -3,7 +3,6 @@ package learnSelenium;
 import java.util.List;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
---
+++ SeleniumBasicsAutomation/src/learnSelenium/SelectableExample.java
@@ -0,0 +1,36 @@
+package learnSelenium;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class SelectableExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "D:\\Automation\\Drivers\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver. get("http://leafground.com/pages/selectable.html");
+		
+		//To get the list of all the ordered List from its ID and append '/li' to the xpath & cross check if the size matches 
+		List<WebElement> selectable = driver.findElements(By.xpath("//*[@id=\'selectable\']/li"));
+		int size = selectable.size();
+		System.out.println(size);
+		
+		//Using Actions class and click and holding the required items by the List order
+		Actions action = new Actions(driver);
+		action
+		.clickAndHold(selectable.get(0))
+		.clickAndHold(selectable.get(1))
+		.clickAndHold(selectable.get(2))
+		.clickAndHold(selectable.get(3));
+		action.build().perform();
+				
+		
+	}
+
+}

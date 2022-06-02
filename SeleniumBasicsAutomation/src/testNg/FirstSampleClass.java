301fab321182b9f6df1de18729f3b1c11b532b74
VinothGnanasekaran <imverypsl4u1@gmail.com>
2022-06-03 00:23:46 +0530

    Commit 10: TestNg Creation as new package

---
+++ SeleniumBasicsAutomation/src/testNg/FirstSampleClass.java
@@ -0,0 +1,28 @@
+package testNg;
+
+import org.testng.annotations.Test;
+
+public class FirstSampleClass {
+	@Test
+	public void startCar () {
+		System.out.println("start the car");
+	}
+	@Test
+	public void putFirstGear () {
+		System.out.println("First Gear");
+	}
+	@Test
+	public void putSecondGear () {
+		System.out.println("Second Gear");
+	}
+	@Test
+	public void putThirdGear () {
+		System.out.println("Third Gear");
+	}
+	@Test
+	public void putFourthGear () {
+		System.out.println("Fourth Gear");
+	}
+	
+	
+}

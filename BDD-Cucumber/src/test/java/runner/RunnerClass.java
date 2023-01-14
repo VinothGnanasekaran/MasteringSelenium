178522e895329f96386122fabcb160d14f314e5b
VinothGnanasekaran <imverypsl4u1@gmail.com>
2023-01-14 12:21:36 +0530

    C-27, 01/14 - updates to Runner Class Documentations

--- BDD-Cucumber/src/test/java/runner/RunnerClass.java
+++ BDD-Cucumber/src/test/java/runner/RunnerClass.java
@@ -7,25 +7,25 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 
-//To learn about Cucumber options 
+//To learn about Cucumber options - Documentation
 /*
 @CucumberOptions (
 		features = "featurefiles" ,
 		glue = "stepdefintions" ,
 		// Boolean - if true-only checks all the step definitions are implemented before actual execution; if false, it executes
 		//Strict= true  - executes first and reports error if any non-implemented steps ; False-No use of defining; Currently not in use.
-		dryRun = true , 
+				dryRun = true , 
 		//Makes the console output in human readable form
-		monochrome = true , 
+				monochrome = true , 
 		// Generates reports of desired format as per JSON/HTML/XML or pretty format syntax said below.
 		// format() - was used earlier, now depricated and Plugin() is suggested
 		//plugin = { "pretty" } - This provides more verbose output.(in console). To implement this, just specify plugin = "pretty" in CucumberOptions. 
-		plugin = {"html:target/cucumber-Webreport", "json:target/cucumber-jsonreport.json", "junit:target/cucumber-xmlReport.xml"},
+				plugin = {"html:target/cucumber-Webreport", "json:target/cucumber-jsonreport.json", "junit:target/cucumber-xmlReport.xml"},
 		//Tags - to group the feature / scenario  as per business needs
 		//This is clubbing of tags by OR Method - if included tags are found, then it will execute those feature/scenarios.
-		tags = "@simpleLogin, @dataDriven, @smoke", 
+				tags = "@simpleLogin, @dataDriven, @smoke", 
 		//This is clubbing of tags in AND Method - if included seperately, then Tags common across feature/scenarios only will be  executed
-		tags = "@simpleLogin" , "@LoginHRM" , "@smoke" 
+				tags = "@simpleLogin" , "@LoginHRM" , "@smoke" 
 		)
 */
 
